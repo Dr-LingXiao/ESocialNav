3751266035f40a01b3843ffe9987ee49326d447a
Ling Xiao <97679887+Dr-LingXiao@users.noreply.github.com>
2025-12-16 22:19:58 +0900

    Add files via upload

---
+++ scripts/train/run_all.sh
@@ -0,0 +1,7 @@
+bash ./scripts/train/train_phi_SNEI-onestage_resampler_aug2.sh
+bash ./scripts/train/train_mof_SNEI.sh && \
+bash ./scripts/train/train_mof_SNEI-onestage.sh && \
+bash ./scripts/train/train_stablelm_SNEI_onestage.sh && \
+bash ./scripts/train/train_stablelm_SNEI.sh && \
+bash ./scripts/train/train_tinyllama_SNEI_onestage.sh && \
+bash ./scripts/train/train_tinyllama_SNEI.sh && \
